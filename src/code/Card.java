79d1f36067a32849356e6df6263815be3ee7b5e8
Nilesh Singh <Nilesh Singh@LAPTOP-I1L0R638>
2017-09-24 16:04:26 -0400

    Deck and Card fully functional.

--- src/code/Card.java
+++ src/code/Card.java
@@ -1,70 +1,14 @@
 package code;
 
+//@author Mark
 public class Card {
 	private String suit;
 	private int rank;
-	private String rb; //color Red or Black
-	
-	public Card(String suit, int rank){
+
+	public Card(String suit, int rank) {
 		this.suit = suit;
 		this.rank = rank;
-		this.rb = rb;
+
 	}
-	
-	public void Heart(){
-		suit = "Heart";
-		rb = "red";
-	}
-	public void Clover(){
-		suit = "Clover";
-		rb = "black";
-	}
-	public void Spade(){
-		suit = "Spade";
-		rb = "black";
-	}
-	public void Diamond(){
-		suit = "Diamond";
-		rb = "red";
-	}
-	
-	public void Two(){
-		rank = 2;
-	}
-	public void Three(){
-		rank = 3;
-	}
-	public void Four(){
-		rank = 4;
-	}
-	public void Five(){
-		rank = 5;
-	}
-	public void Six(){
-		rank = 6;
-	}
-	public void Seven(){
-		rank = 7;
-	}
-	public void Eight(){
-		rank = 8;
-	}
-	public void Nine(){
-		rank = 9;
-	}
-	public void Ten(){
-		rank = 10;
-	}
-	public void Jack(){
-		rank = 11;
-	}
-	public void Queen(){
-		rank = 12;
-	}
-	public void King(){
-		rank = 13;
-	}
-	public void Ace(){
-		rank = 1;
-	}
+
 }
---
+++ src/code/Deck.java
@@ -0,0 +1,72 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+	Card[] deck;
+
+	public Deck() {
+		deck = new Card[52];
+	}
+	// @ co-author Mark & Nilesh
+
+	public Deck getDeck() {
+
+		int j = 0;
+		for (int i = 1; i < 14; i++) {
+			deck[j] = new Card(Spade(), i);
+			j++;
+		}
+		for (int i = 1; i < 14; i++) {
+			deck[j] = new Card(Heart(), i);
+			j++;
+		}
+		for (int i = 1; i < 14; i++) {
+			deck[j] = new Card(Clover(), i);
+			j++;
+		}
+		for (int i = 1; i < 14; i++) {
+			deck[j] = new Card(Diamond(), i);
+			j++;
+		}
+
+		unique();
+		return this;
+	}
+
+	// @Mark
+	public String Heart() {
+		return "Heart";
+
+	}
+
+	public String Clover() {
+		return "Clover";
+
+	}
+
+	public String Spade() {
+		return "Spade";
+
+	}
+
+	public String Diamond() {
+		return "Diamond";
+	}
+
+	// @Nilesh
+	public void unique() {
+		ArrayList<Card> a = new ArrayList<Card>();
+
+		for (int i = 0; i < 52; i++) {
+			a.add(deck[i]);
+		}
+		Collections.shuffle(a);
+		for (int i = 0; i < 52; i++) {
+			deck[i] = a.get(i);
+		}
+
+	}
+}
