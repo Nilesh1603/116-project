cee714c6fd249a542b08095a336662a4a05975ed
stimei <mark.sigalov@gmail.com>
2017-09-29 19:15:03 -0400

    created deck and card test class

--- src/tests/deckAndCardClassTest.java
+++ src/tests/deckAndCardClassTest.java
@@ -1,12 +1,30 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Card;
 import code.Deck;
 
 public class deckAndCardClassTest {
 
-	// @Test
+	@Test
 	public void definesDeckClass() {
+		boolean check = false;
 		Deck a = new Deck();
-		a.getDeck();
+		Card[] ch = a.getDeck();
+		for(int i=0; i < 52; i++){
+			Card b = ch[i];
+			for(int j=1; j<52;j++){
+				Card c = ch[j];
+				if(b.getRank() != c.getRank() ||! b.getSuit().equals(c.getSuit())){
+					check = true;
+					assertTrue(check);
+					
+				}
+			}
+			
+		}
 	}
 }
