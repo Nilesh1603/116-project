5e82d5daf44d4fcb458be23efc499ceb917fc19d
Nabeel <nkhalid@buffalo.edu>
2017-09-29 20:42:15 -0400

    started tab

--- src/tests/tableauPileTests.java
+++ src/tests/tableauPileTests.java
@@ -1,9 +1,18 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import code.Freecell;
+//@author Nabeel
 public class tableauPileTests {
 
-
+	public void checkTableauPilesInFreeCell(){
+		Freecell a= new Freecell();
+		assertEquals("CheckTableauPile.size()", 6, a.Tableau.size());
+	
+		
+	}
 
 }
