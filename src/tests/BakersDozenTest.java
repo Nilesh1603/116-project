df5d21d1dd5852eaffc17457ced2e353dc0c4b72
Nabeel <nkhalid@buffalo.edu>
2017-09-29 20:20:21 -0400

    added BakersDozenTest

--- src/tests/BakersDozenTest.java
+++ src/tests/BakersDozenTest.java
@@ -1,8 +1,25 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.BakersDozengame;
+import code.Freecell;
+
 public class BakersDozenTest {
-	
+	//@Nabeel
 	//Each game instance has
 	//13 tableau piles, 4 homecell piles, and 0 freecell piles [6 points]
-
+	@Test
+	public void checkTableauPiles(){
+		BakersDozengame a= new BakersDozengame();
+		assertEquals("CheckTableauPile.size()", 13, a.Tableau.size());
+	}
+	
+	@Test 
+	public void checkHomeCell(){
+		BakersDozengame a= new BakersDozengame();
+			assertEquals("CheckHomeCell.size()", 4, a.HomeCell.size());
+	}
 }
--- src/tests/FreecellTest.java
+++ src/tests/FreecellTest.java
@@ -5,19 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import code.Freecell;
-
+//@author Nabeel Khalid
 public class FreecellTest {
 	//Each game instance has 8 tableau piles, 4 homecell piles, and 4 freecell piles 
 	
 	
 	@Test
 	public void checkTableauPiles(){
-		
 		Freecell a= new Freecell();
 		assertEquals("CheckTableauPile.size()", 8, a.Tableau.size());
 	
 		
 	}
+	@Test 
+	public void checkHomeCellPiles(){
+			Freecell a= new Freecell();
+			assertEquals("CheckFreeCell.size()", 4, a.HomeCell.size());
+		
+			
+		}
+	@Test 
+	public void checkFreeCell(){
+			Freecell a= new Freecell();
+			assertEquals("CheckHomeCell.size()", 4, a.FreeCell.size());
+	}
+}
 	
 
-}
+
