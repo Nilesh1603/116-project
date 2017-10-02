714d940d22cdc5f2e2b96996d480814e94158d9b
stimei <mark.sigalov@gmail.com>
2017-10-02 19:23:19 -0400

    Started comments

--- src/code/Card.java
+++ src/code/Card.java
@@ -1,25 +1,30 @@
 package code;
 
 //@author Mark
-//@author Nabeel
-
-/**
- * 
- * Defines a specific card
- *
- */
 public class Card {
 	private String suit;
 	private int rank;
 
+	/**
+	 * 
+	 * @param suit represents the suit of the card (hearts, diamonds, clubs, spades)
+	 * @param rank represents the rank of the card (Ace, 2-10, Jack, Queen, King)
+	 */
 	public Card(String suit, int rank) {
 		this.suit = suit;
 		this.rank = rank;
-
 	}
+	/**
+	 * 
+	 * @return suit of the card
+	 */
 	public String getSuit(){
 		return this.suit;
 	}
+	/**
+	 * 
+	 * @return rank of the card
+	 */
 	public int getRank(){
 		return this.rank;
 	}
